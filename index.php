4ca30a716fec6bd4a1145acbe4cd88e8ce8313ac
Python The Hack <lrulvoni@gmail.com>
2022-01-13 11:04:58 +0100

    update dashboard

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
                     </div>
                     <?php
                     // Include config file
-                    require_once "config.php";
+                    require_once "./include/config.php";
                     
                     // Attempt select query execution
                     $sql = "SELECT * FROM employees";
@@ -56,7 +56,7 @@
                                         echo "<td>" . $row['id'] . "</td>";
                                         echo "<td>" . $row['name'] . "</td>";
                                         echo "<td>" . $row['address'] . "</td>";
-                                        echo "<td>" . $row['salary'] . "</td>";
+                                        echo "<td>" ."<b>€</b> " . $row['salary'] . "</td>";
                                         echo "<td>";
                                             echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="Vedi Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
                                             echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
--- read.php
+++ read.php
@@ -2,7 +2,7 @@
 // Check existence of id parameter before processing further
 if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     // Include config file
-    require_once "config.php";
+    require_once "./include/config.php";
     
     // Prepare a select statement
     $sql = "SELECT * FROM employees WHERE id = :id";
